075b22795323ea994c655575072d15624e93349a
georgijbudik <georgijbudik@gmail.com>
2023-10-25 23:09:18 +0200

    finish hw

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,7 @@
-let some: string;
+let some: unknown;
 some = "Text";
 let str: string;
-str = some;
-
+if (typeof some === "string") {
+  str = some;
+}
 export {};
--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,9 +3,15 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T extends string | number[]>(): Promise<T> {
+// function getPromise<T extends string | number[]>(): Promise<T> {
+//   return new Promise((resolve) => {
+//     resolve(["Text", 50] as T);
+//   });
+// }
+
+function getPromise(): Promise<[string, number]> {
   return new Promise((resolve) => {
-    resolve(["Text", 50] as T);
+    resolve(["Text", 50]);
   });
 }
 
